78c89a4791619ff6edf1806353628c0e59de107f
Eric Anderson <anderson.eda@gmail.com>
2019-07-17 14:44:17 -0400

    first commit

---
+++ src/components/LoginPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { startLogin } from '../actions/auth';
+
+export const LoginPage = ({ startLogin }) => (
+  <div className="box-layout">
+  <div className="box-layout__box">
+    <h1 className="box-layout__title">Watermelon</h1>
+    <p>Water Hotter.</p>
+    <button className="button" onClick={startLogin}>Login with Google</button>
+  </div>
+  </div>
+);
+
+const mapDispatchToProps = (dispatch) => ({
+  startLogin: () => dispatch(startLogin())
+})
+
+export default connect(undefined, mapDispatchToProps)(LoginPage);
+
